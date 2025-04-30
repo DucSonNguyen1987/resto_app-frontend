d2f680618e086c9dd1656cbdf1cdb5edf7c03088
Duc Son Nguyen <duc.son.nguyen48@gmail.com>
2025-04-30 12:51:05 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+// Imports redux
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer} from 'redux-persist';
+import { PersistGate} from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {injectStore} from '../api/axios';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +16,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Hello</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
